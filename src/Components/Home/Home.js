42aeb0af8647ba78f50c763b7577ef49ddce1b40
DilmaTomin <dilma.joy@jobinandjismi.com>
2023-04-15 01:20:51 +0530

    home-build-slider

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,10 +2,12 @@ import { Typography } from '@mui/material'
 import React from 'react'
 import './Home.css'
 import Home_section3 from './Home_section3'
+import HomeBuildCarrier from './HomeBuildCarrier'
 
 const Home = () => {
   return (
     <div>
+       <HomeBuildCarrier></HomeBuildCarrier>
         <Home_section3></Home_section3>
       
     </div>
