1b872663a3a5c1ca06e8c5e639e2695efb385cb9
anand93r <anand2007r@gmail.com>
2023-04-27 22:16:15 +0530

    contactus start

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,31 @@
-import { Typography } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import React from 'react'
+import './Home/Home.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const Contact = () => {
   return (
     <div>
-      <Typography className='con' variant='h1'>Contact Page</Typography>
+            <div class="Home-Builder-Carrier">
+                <div class="Home-Builder-Carrier-container">
+                    <div class='row'>
+                        <div class='col-md-6' >
+                            <div class="Home-builder-carrier-content">
+                                <h2 class="Home-builder-carrier-title">Get In Touch</h2>
+                                
+                            </div>
+                            
+                        </div>
+                        <div class='col-md-1' >
+                            <div class="Home-builder-carrier-image">
+                                <img src='	https://ictkerala.org/assets/images/GRA_Home-Header-Overlay.png' className='Homebuildercarrier-img'></img>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
     </div>
   )
 }
